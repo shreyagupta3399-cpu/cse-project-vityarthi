7bc9529ebacb03fe4a6dad24f89a0b8a7e741fab
shreyagupta3399-cpu <shreyagupta3399@gmail.com>
2025-11-24 17:51:31 +0530

    output..py

---
+++ output..py
@@ -0,0 +1,37 @@
+--- Personal Finance Manager ---
+1. Add Income
+2. Add Expense
+3. Show Balance
+4. Exit
+Enter your choice: 1
+Enter income amount: 50000
+Income added!
+
+--- Personal Finance Manager ---
+1. Add Income
+2. Add Expense
+3. Show Balance
+4. Exit
+Enter your choice: 2
+Enter expense amount: 10000
+Expense added!
+
+--- Personal Finance Manager ---
+1. Add Income
+2. Add Expense
+3. Show Balance
+4. Exit
+Enter your choice: 3
+
+---- Summary ----
+Total Income: 50000.0
+Total Expense: 10000.0
+Balance: 40000.0
+
+--- Personal Finance Manager ---
+1. Add Income
+2. Add Expense
+3. Show Balance
+4. Exit
+Enter your choice: 4
+Exiting... Goodbye!
